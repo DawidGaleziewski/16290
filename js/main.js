f76e71ca397382dd7c704d6a058725e34a099f0a
Dawid Galeziewski <Dawid.Galeziewski@ivanti.com>
2019-04-03 15:34:55 +0200

    added onclick event to the button

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-alert('e')
+let opis = document.getElementById('opis');
+let button = document.querySelector('button');
+
+button.onclick = function(){
+        opis.innerText = 'Akademia 108';
+    }
